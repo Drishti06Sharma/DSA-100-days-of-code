bbeecdd789879f10f0452efd83ee665a1fbdb9eb
Drishti Sharma <20drishti06@gmail.com>
2026-02-25 08:15:31 +0000

    dsa

---
+++ CODE_25/day25.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure definition
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to count occurrences of key
+int countOccurrences(struct Node* head, int key) {
+    int count = 0;
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        if (temp->data == key) {
+            count++;
+        }
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+int main() {
+    int n, value, key;
+    struct Node *head = NULL, *tail = NULL;
+
+    scanf("%d", &n);
+
+    // Creating linked list
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        struct Node* newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    scanf("%d", &key);
+
+    int result = countOccurrences(head, key);
+
+    printf("%d", result);
+
+    return 0;
+}
