e9207386f5e9636ab596ead2da2412ff2279846d
Drishti Sharma <20drishti06@gmail.com>
2026-03-04 16:03:13 +0000

    dsa

---
+++ CODE_31/day31.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100   // Maximum stack size
+
+int stack[MAX];
+int top = -1;
+
+/* Push operation */
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack[++top] = value;
+}
+
+/* Pop operation */
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    printf("%d\n", stack[top--]);
+}
+
+/* Display operation */
+void display() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    for (int i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, type, value;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &type);
+
+        if (type == 1) {
+            scanf("%d", &value);
+            push(value);
+        }
+        else if (type == 2) {
+            pop();
+        }
+        else if (type == 3) {
+            display();
+        }
+    }
+
+    return 0;
+}
